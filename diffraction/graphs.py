951904a83667c654a636b2a749c768b53dbbac16
Mathieu Marquis <mamar828@ulaval.ca>
2024-01-29 23:21:00 -0500

    4 graphs and width/distance calculations

--- diffraction/graphs.py
+++ diffraction/graphs.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
+from eztcolors import Colors as C
+import uncertainties
+
 m_per_pixel = 2.8e-5                # 0.007 * 11 / 2712
 
 
@@ -57,33 +60,36 @@ def save_figure(
 
 
 # save_figure(
-#     "diffraction/data/Diffraction_smol.csv",
+#     "diffraction/data/cer.csv",
 #     "position [m]",
 #     "intensité [-]",
-#     "Intensité mesurée en fonction de la position dans une\nfigure de diffraction produite par un laser à 650 nm",
-#     # "diffraction/figures/diffraction_smol.png"
+#     ("Intensité en fonction de la position dans une figure de diffraction produite\navec un laser à 650 nm "+
+#      "par une ouverture circulaire d'un diamètre de 0.2 mm"),
+#     "diffraction/figures/cer.png"
 # )
 
 
-def find_width(filename):
-    vals = get_csv_data(filename)
-    # vals *= np.tile(np.array([m_per_pixel, 1]), (vals.shape[0],1))
-    plt.plot(vals[:,0], vals[:,1], "b-")
-    plt.show()
+def find_peak_distance(mins: list):
+    y = np.diff(mins) * m_per_pixel
+    # Filter aberrant data and compute mean value
+    real_y = y[y<np.mean(y)+np.std(y)]
+    mean_distance = uncertainties.ufloat(np.mean(real_y), np.std(real_y))
+    lambd = uncertainties.ufloat(650,1)*1e-9
+    L = uncertainties.ufloat(1,0.01)
+    distance = lambd / (mean_distance / L)
+    print(f"{C.UNDERLINE}Mean min distance{C.END}: {C.BOLD}{C.YELLOW}{mean_distance:.1u} m{C.END}".replace(
+                                                                                                        "+/-"," ± "))
+    print(f"{C.UNDERLINE}Measure{C.END}:           {C.BOLD}{C.LIGHT_BLUE}{distance:.1u} m{C.END}".replace("+/-"," ± "))
 
 
+print(f"\n{C.NEGATIVE}Single slit 0.08mm{C.END}")
+find_peak_distance([922, 1238, 1592, 1961, 2319, 2665, 2996, 3631, 3975, 4309, 4653, 4953, 5243, 5575, 5841, 6089])
 
+print(f"\n{C.NEGATIVE}Double slits 0.08mm, distance between slits{C.END}")
+find_peak_distance([2593, 2693, 2795, 2891, 2990, 3089, 3185, 3276, 3369, 3466, 3555, 3653, 3745, 
+                    3840, 3930, 4019, 4112, 4203, 4289, 4379, 4473, 4553, 4646, 4737, 4827, 4911, 5001, 5108])
+print(f"\n{C.NEGATIVE}Double slits 0.08mm, slit width{C.END}")
+find_peak_distance([503, 1211, 1897, 2593, 5108, 5737, 6265, 6737])
 
-
-find_width("diffraction/data/Diffraction_smol.csv")
-
-
-# def get_interference_figure():
-#     vals = get_csv_data("diffraction/interference_beau_avg.csv")
-#     vals *= np.tile(np.array([m_per_pixel, 1]), (vals.shape[0],1))
-
-#     plt.plot(vals[:,0], vals[:,1], "r-", markersize=1)
-#     plt.show()
-
-
-# get_interference_figure()
+print(f"\n{C.NEGATIVE}Spherical opening{C.END}")
+find_peak_distance([845, 958, 1069, 1159, 1277, 1530, 1627, 1798, 1798, 1932, 2034])
