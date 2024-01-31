9b0edc0b3df652e26d684d604aac9596d1b6489e
Mathieu Marquis <mamar828@ulaval.ca>
2024-01-31 11:49:31 -0500

    mergeeee

--- bruit/main.py
+++ bruit/main.py
@@ -37,7 +37,7 @@ def get_intensity_increase(noise_filename: str, reference_filename: str) -> unce
 # print(
 #     (f"{C.NEGATIVE}Intensity increase{C.END}: " +
 #     str(get_intensity_increase(noise_filename='bruit/data/F0002CH1.CSV', reference_filename='bruit/data/F0002CH2.CSV'))
-#     ).replace("+/-", " ± ")
+#     )
 # )
 
 
@@ -62,3 +62,38 @@ def analyze_multiple_csv(data_dir: str) -> Union[uncertainties.core.Variable, np
 
 
 print(f"{analyze_multiple_csv('./data'):.1u}")
+
+
+
+def analyze_multiple_csv():
+    pass
+
+
+
+def make_graph():
+    noise_data = get_csv_data("bruit/data/F0002CH1.CSV")
+    reference_data = get_csv_data("bruit/data/F0002CH2.CSV")
+
+    high_low_threshold = 1      # Set the intensity between high and low regimes
+    high_data = noise_data[reference_data[:,1] > high_low_threshold]
+    low_data = noise_data[reference_data[:,1] < high_low_threshold]
+
+    fig, axs = plt.subplots(1,2)
+    fig.set_size_inches(10,5)
+
+    fig.supxlabel("Temps [unités arbitraires]")
+    fig.supylabel("Intensité du signal [V]")
+    axs[0].plot(high_data[:70,0], high_data[:70,1], "g-", markersize=0.25, label="Régime actif")
+    axs[0].plot(high_data[70:,0], high_data[70:,1], "g-", markersize=0.25)
+    axs[0].plot(low_data[:1000,0], low_data[:1000,1], "b-", markersize=0.25, label="Régime passif")
+    axs[0].plot(low_data[1000:,0], low_data[1000:,1], "b-", markersize=0.25)
+    axs[0].legend()
+
+    axs[1].hist(low_data[:,1], bins=[0.68,0.690,0.7,0.71,0.715,0.725,0.73,0.74])
+    axs[1].hist(high_data[:,1], bins=[0.735,0.74,0.75,0.755,0.765,0.77,0.78])
+
+    # plt.savefig()
+    plt.show()
+
+
+# make_graph()
