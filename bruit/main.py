c3911bf2934333c61ed6ecd64d736853548f7bcc
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2024-01-31 11:47:16 -0500

    added data and regex file finding

--- bruit/main.py
+++ bruit/main.py
@@ -1,9 +1,13 @@
+import os
+import re
+from typing import Union
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import uncertainties.core
 from uncertainties import ufloat
 from eztcolors import Colors as C
-from pprint import pprint
 
 
 def get_csv_data(filename: str) -> np.ndarray:
@@ -12,7 +16,7 @@ def get_csv_data(filename: str) -> np.ndarray:
     return arr[:,3:5].astype(float)
 
 
-def get_intensity_increase(noise_filename: np.ndarray, reference_filename: np.ndarray):
+def get_intensity_increase(noise_filename: str, reference_filename: str) -> uncertainties.core.Variable:
     noise_data = get_csv_data(noise_filename)
     reference_data = get_csv_data(reference_filename)
 
@@ -23,7 +27,7 @@ def get_intensity_increase(noise_filename: np.ndarray, reference_filename: np.nd
     plt.plot(reference_data[:,0], reference_data[:,1], "ro", markersize=0.5)
     plt.plot(high_data[:,0], high_data[:,1], "go", markersize=0.5)
     plt.plot(low_data[:,0], low_data[:,1], "bo", markersize=0.5)
-    # plt.show()
+    plt.show()
 
     high_val = ufloat(np.nanmean(high_data[:,1]), np.nanstd(high_data[:,1]))
     low_val = ufloat(np.nanmean(low_data[:,1]), np.nanstd(low_data[:,1]))
@@ -36,8 +40,25 @@ def get_intensity_increase(noise_filename: np.ndarray, reference_filename: np.nd
 #     ).replace("+/-", " ± ")
 # )
 
-pprint(u"{ufloat(3,2)}")
+
+def analyze_multiple_csv(data_dir: str) -> Union[uncertainties.core.Variable, np.ndarray]:
+    # any_patern = re.compile(r"^F\d{4}CH\d.CSV$")
+    # nb_files = len([
+    #     name for name in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, name)) and any_patern.match(name)
+    # ])
+    noise_patern = re.compile(r"^F\d{4}CH1.CSV$")
+    intensity_increase_list = []
+    for noise_file_name in os.listdir(data_dir):
+        if os.path.isfile(os.path.join(data_dir, noise_file_name)) and noise_patern.match(noise_file_name):
+            current_data_patern = re.compile(noise_file_name[:-5]+"2.CSV")
+            for ref_file_name in os.listdir(data_dir):
+                if os.path.isfile(os.path.join(data_dir, ref_file_name)) and current_data_patern.match(ref_file_name):
+                    intensity_increase_list += [get_intensity_increase(
+                        os.path.join(data_dir, noise_file_name),
+                        os.path.join(data_dir, ref_file_name)
+                    )]
+    print(intensity_increase_list)
+    return np.mean(intensity_increase_list)
 
 
-def analyze_multiple_csv():
-    pass
+print(f"{analyze_multiple_csv('./data'):.1u}")
