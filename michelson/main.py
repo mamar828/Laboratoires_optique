b49a5cc67845b496845798f80225b2c629ae4032
Mathieu Marquis <mamar828@ulaval.ca>
2024-02-13 14:50:53 -0500

    final michelson pt1

--- michelson/main.py
+++ michelson/main.py
@@ -20,18 +20,20 @@ def plot(filename):
 # plot("michelson/data_csv/laser.csv")
 # plot("michelson/data_csv/tungsten.csv")
 # plot("michelson/data_csv/HG1.csv")
-    
+
 
 def find_DPZ():
     # Uncertainties for the relative dpz and the start of the calibration are chosen to be half of the data steps
+    # All values are given in μm
     data = get_csv_data("michelson/data_csv/tungsten.csv")
-    relative_dpz = ufloat(data[np.argmin(data[:,2]),1], np.diff(data[:2,1])/2)
-    
-    calib_start = ufloat(1.6555700E+5/9.8, np.diff(data[:2,1])/2)
+    unc = (6.4087213 - -3.9889987) / 1.0280000E+3 / 2 * 10          # times 10 to correct the uncertainty
+    relative_dpz = ufloat(data[np.argmin(data[:,2]),1], unc)
+    calib_start = ufloat(1.6555700E+5/9.8 + 3.9889987, unc)
     return relative_dpz + calib_start
 
 
 # print(f"DPZ: {find_DPZ()} μm")
+# DPZ: 16898.51 ± 0.07 μm
 
 
 def make_graph():
@@ -39,20 +41,32 @@ def make_graph():
     fig.set_size_inches(9,6)
     DPZ = find_DPZ().nominal_value
 
-    calibrations = [
+    abs_calib = np.array([
         1.6493300E+5 / 9.8,
         1.6555700E+5 / 9.8,
         1.5377300E+5 / 9.8
-    ]
+    ])
+
+    zero_point = np.array([
+        1.0306601E+1,
+        3.9889987,
+        6.8855788E+1
+    ])
+
+    x_offsets = abs_calib + zero_point - DPZ
     
+    # Get arrays and remove the relative calibrations to calibrate in absolute
     raw_arrays = [
         get_csv_data("michelson/data_csv/laser.csv"),
         get_csv_data("michelson/data_csv/tungsten.csv"),
         get_csv_data("michelson/data_csv/HG2.csv")
     ]
 
-    aligned_arrays = [raw_array + calibration - DPZ for raw_array, calibration in zip(raw_arrays, calibrations)]
-
+    aligned_arrays = []
+    for raw_array, x_offset in zip(raw_arrays, x_offsets):
+        x_size = raw_array.shape[0]
+        aligned_arrays.append(np.tile([0,x_offset,0], (x_size,1)) + raw_array)
+    
     # Set subplot labels
     for row, text in zip(range(3), ["a)","b)","c)"]):
         axs[row].text(0.01, 0.83, text, fontsize=15, transform=axs[row].transAxes)
@@ -63,10 +77,25 @@ def make_graph():
         fig.supxlabel(r"Différence de parcours [$\mu$m]")
         fig.supylabel("Intensité du signal [mV]")
 
-        axs[row_number].plot(array[:,1], array[:,2], "bo", markersize=0.25)
+        axs[row_number].plot(array[:,1], array[:,2], "b-", markersize=0.25)
 
     # plt.savefig("michelson/figures/coherent_distance.png", dpi=300, bbox_inches="tight")
     plt.show()
 
 
-make_graph()
+# make_graph()
+
+
+def find_tungsten_coherent_distance():
+    shape = 200, 5
+    threshold = 3
+    data = get_csv_data("michelson/data_csv/tungsten.csv")[:-28,:]     # Get only 1000 points
+    mean_intensity = np.mean(data[:,2].reshape(*shape), axis=1)
+    mean_position = np.mean(data[:,1].reshape(*shape), axis=1)[:-1]
+
+    print(mean_position[np.abs(np.diff(mean_intensity)) > threshold])
+    
+    unc = 0.3
+    print(ufloat(2, unc) - ufloat(-2, unc))
+
+# find_tungsten_coherent_distance()
