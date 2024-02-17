001c6d0939e4863edd6e812a063dc771fc2c72c2
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2024-02-16 20:47:57 -0500

    added second lab data

--- michelson/main.py
+++ michelson/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from numpy.random import *
+from numpy.fft import *
 
 def get_csv_data(filename: str) -> np.ndarray:
     df = pd.read_csv(filename, sep=',', header=None)
@@ -14,11 +15,19 @@ def plot(filename):
     plt.plot(data[:,0], data[:,2], "g-", markersize=2)
     plt.show()
 
+def fourierTransformInterferogram(x,y):
+	spectrum = fft(y)
+	dx = x[1]-x[0] # on obtient dx, on suppose equidistant
+	N = len(x)     # on obtient N directement des données
+	frequencies = fftfreq(N, dx) # Cette fonction est fournie par numpy
+	wavelengths = 1/frequencies  # Les fréquences en µm^-1 sont moins utiles que lambda en µm
+	return (wavelengths, frequencies, spectrum)
 
-
-# plot("michelson/data_csv/tungsten.csv")
-# plot("michelson/data_csv/laser.csv")
-# plot("michelson/data_csv/HG3.csv")
+plot("/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/Michelson/data_csv/tungsten.csv")
+plot("/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/Michelson/data_csv/laser.csv")
+plot("/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/Michelson/data_csv/HG3.csv")
+plot("/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/Michelson/data_csv/HG2.csv")
+plot("/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/Michelson/data_csv/HG1.csv")
 
 # plt.plot(np.random.random(10**6), np.random(10**6))
 # plt.show()
@@ -31,5 +40,3 @@ def plot(filename):
 
 # print(get_csv_data("michelson/data_csv/HG1.csv"))
     
-
-
