8bbdb118c739c56d276604311a389bb8092ab46f
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2024-03-24 11:21:02 -0400

    more images for different filters

--- project_1/main.py
+++ project_1/main.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-from project_1.video import Video
+# from project_1.video import Video
+from video import Video
 
 
 def plot(array, save_filename=None):
@@ -52,8 +53,8 @@ def autoextract_videos():
 
 
 def compute_mean_motion_energy(video_name):
-    video = Video(f"project_1/video_data/{video_name}.MP4")
-    with open(f"project_1/crop_slices/{video_name.split('.')[0]}.txt", "r") as f:
+    video = Video(f"/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/project_1/video_data/{video_name}.MP4")
+    with open(f"/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/project_1/crop_slices/{video_name.split('.')[0]}.txt", "r") as f:
         lines = f.read().split("\n")[1:]
         x_slice = slice(int(lines[0].split("(")[1].split(",")[0]), int(lines[0].split(", ")[1].split(",")[0]))
         y_slice = slice(int(lines[1].split("(")[1].split(",")[0]), int(lines[1].split(", ")[1].split(",")[0]))
@@ -61,8 +62,11 @@ def compute_mean_motion_energy(video_name):
     video.convert_to_luminosity()
 
     motion_energy = video.get_mean_motion_energy()
-    cutted_motion_energy = Video.get_filtered_array(motion_energy, 0.25 * (np.max(motion_energy) - np.min(motion_energy)) + np.min(motion_energy))
-    plot(cutted_motion_energy, f"project_1/figures/{video_name.split('.')[0]}_filtered.png")
+    print(np.std(motion_energy/np.max(motion_energy)), np.mean(motion_energy))
+    cutted_motion_energy = Video.get_filtered_array(motion_energy, 0.15 * (np.max(motion_energy) - np.min(motion_energy)) + np.min(motion_energy))
+    print(np.nanstd(cutted_motion_energy/np.nanmax(cutted_motion_energy)), np.nanmean(cutted_motion_energy))
+    # plot(cutted_motion_energy, f"/Users/felixdesroches/Desktop/ULaval_labs/PHY_2006_optique/Laboratoires_optique/project_1/figures/{video_name.split('.')[0]}_filtered_20.png")
 
 
-compute_mean_motion_energy("25_degree")
+compute_mean_motion_energy("jet_seul")
+compute_mean_motion_energy("vide")
